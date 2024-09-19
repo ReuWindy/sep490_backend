ad021af70cd42771c5e127716d6bb07aeb9b4144
ReuWindy <daoanhtu2k2@gmail.com>
2024-09-19 08:28:07 +0700

    Setup

---
+++ src/main/java/com/fpt/sep490/FinalApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.sep490;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/sep490/FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.sep490;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
