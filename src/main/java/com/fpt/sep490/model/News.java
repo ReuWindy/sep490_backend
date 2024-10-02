667029271fa719467ff5851f227e7de15ae5894b
Nam <toquangnam2003@gmail.com>
2024-10-02 12:57:00 +0700

    Update,modify database

---
+++ src/main/java/com/fpt/sep490/model/News.java
@@ -0,0 +1,49 @@
+package com.fpt.sep490.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "news")
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "NVARCHAR(1000)")
+    private String name;
+
+    @Column(nullable = true)
+    private String image;
+
+    @Column(columnDefinition = "NVARCHAR(1000)")
+    private String type;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String description;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+    @Column(nullable = false)
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+}
