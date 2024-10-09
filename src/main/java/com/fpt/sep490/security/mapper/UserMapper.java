8868d0e22dfeac71c65ebcdf840f1a630f221040
Nam <toquangnam2003@gmail.com>
2024-10-09 21:51:50 +0700

    Update: UserMapper

--- src/main/java/com/fpt/sep490/security/mapper/UserMapper.java
+++ src/main/java/com/fpt/sep490/security/mapper/UserMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
-@Component
+
 public interface UserMapper {
     //UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
