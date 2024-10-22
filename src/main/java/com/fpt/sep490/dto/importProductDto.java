be9c6e50c425ca80fb377e006e23bdf18231628c
edward <daoanhtu2k2@gmail.com>
2024-10-22 20:03:05 +0700

    Add: Import ProductDto

---
+++ src/main/java/com/fpt/sep490/dto/importProductDto.java
@@ -0,0 +1,19 @@
+package com.fpt.sep490.dto;
+
+import lombok.Data;
+
+@Data
+public class importProductDto {
+    private String name;
+    private String description;
+    private double importPrice;
+    private String image;
+    private int quantity;
+    private double weight;
+    private String unit;
+    private String categoryId;
+    private Long supplierId;
+    private Long unitOfMeasureId;
+    private Long warehouseId;
+    private String productUnit;
+}
